8a7cdea45ed971b0acec52694c689cc5fd6d2a1f
yashchaurasia667 <yashchaurasia630@gmail.com>
2024-02-20 15:27:48 +0530

    completed malloc and free

--- malloc.c
+++ malloc.c
@@ -19,6 +19,16 @@ typedef union header header_t;
 header_t *head, *tail;
 pthread_mutex_t global_malloc_lock;
 
+header_t *get_free_block(size_t size) {
+    header_t *curr = head;
+    while(curr) {
+        if(curr->s.is_free && curr->s.size >= size) 
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
+}
+
 void *malloc(size_t size) {
     size_t total_size;
     void *block;
@@ -52,12 +62,34 @@ void *malloc(size_t size) {
     return (void *)(header + 1);
 }
 
-header_t *get_free_block(size_t size) {
-    header_t *curr = head;
-    while(curr) {
-        if(curr->s.is_free && curr->s.size >= size) 
-            return curr;
-        curr = curr->s.next;
+void free(void *block) {
+    header_t *header, *tmp;
+    void *programbreak;
+
+    if(!block) 
+        return ;
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (header_t*)block-1;
+
+    programbreak = sbrk(0);
+    if((char*)block + header->s.size == programbreak) {
+        if(head == tail) {
+            head = tail = NULL;
+        }
+        else {
+            tmp = head;
+            while(tmp) {
+                    if(tmp->s.next == tail) {
+                        tmp->s.next = NULL;
+                        tail = tmp;
+                    }
+                    tmp = tmp->s.next;
+            }
+        }
+        sbrk(0-sizeof(header_t) - header->s.size);
+        pthread_mutex_unlock(&global_malloc_lock);
+        return;
     }
-    return NULL;
+    header->s.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
 }
